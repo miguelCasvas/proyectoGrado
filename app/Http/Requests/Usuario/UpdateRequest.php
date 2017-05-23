d44e441091e3f45880500c73df2a16c1394a5316
miguel.castaneda <miguel.castaneda@parservicios.com>
2017-05-23 07:47:31 -0500

    Modificaciones en clases request para generar respuesta JSON

--- app/Http/Requests/Usuario/UpdateRequest.php
+++ app/Http/Requests/Usuario/UpdateRequest.php
@@ -2,19 +2,11 @@
 
 namespace App\Http\Requests\Usuario;
 
+use App\Http\Requests\FormRequestToAPI\FormRequestToAPI;
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateRequest extends FormRequest
+class UpdateRequest extends FormRequestToAPI
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
--- resources/lang/en/errors.php
+++ resources/lang/en/errors.php
@@ -4,5 +4,6 @@ return [
     200 => 'Success',
     400 => 'Bad Request',
     404 => 'Not Found',
-    901 => 'Id Not Found'
+    901 => 'Id Not Found',
+    902 => ''
 ];
